45de4375630be717486677a5a02e34fade3353ee
PauloAndradeOliveira <pcoliveiraandrade@outlook.com>
2025-12-01 19:09:33 -0300

    Atribuindo valores

---
+++ criando-primeira-aplicacao/Screen-Match/src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Bem Vindo ao Screen Match");
+        System.out.println("Filme: Top Gun");
+
+        // declarando variáveis.
+        int anoDeLancamento = 2024;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = false; // Diz se está incluido no pláno.
+        Double notaDoFilme = 8.0;// Nota do filme, usamos o Double => Ponto flutuante.
+
+
+
+
+    }
+}
